c101ab0fa290062eca279fcab8f25b13bbc2a066
Sönke Liebau <soenke.liebau@gmail.com>
2021-09-09 20:37:33 +0200

    fixes #40

--- rust/operator-binary/build.rs
+++ rust/operator-binary/build.rs
@@ -4,7 +4,7 @@ use stackable_operator::crd::CustomResourceExt;
 fn main() -> Result<(), stackable_operator::error::Error> {
     built::write_built_file().expect("Failed to acquire build-time information");
 
-    MonitoringCluster::write_yaml_schema("../deploy/crd/monitoringcluster.crd.yaml")?;
+    MonitoringCluster::write_yaml_schema("../../deploy/crd/monitoringcluster.crd.yaml")?;
 
     Ok(())
 }
